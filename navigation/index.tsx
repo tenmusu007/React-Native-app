0fcc1b22350b3d985a149716f7c9799426b8527a
Atsuya Tanaka <tenmusu007@gmail.com>
2023-01-08 23:45:03 -0800

    init

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -57,43 +57,60 @@ function BottomTabNavigator() {
   const colorScheme = useColorScheme();
 
   return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
-      <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+		<BottomTab.Navigator
+			initialRouteName='TabOne'
+			screenOptions={{
+				tabBarActiveTintColor: Colors[colorScheme].tint,
+			}}
+		>
+			<BottomTab.Screen
+				name='TabOne'
+				component={TabOneScreen}
+				options={({ navigation }: RootTabScreenProps<"TabOne">) => ({
+					title: "Tab One",
+					tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+					headerRight: () => (
+						<Pressable
+							onPress={() => navigation.navigate("Modal")}
+							style={({ pressed }) => ({
+								opacity: pressed ? 0.5 : 1,
+							})}
+						>
+							<FontAwesome
+								name='info-circle'
+								size={25}
+								color={Colors[colorScheme].text}
+								style={{ marginRight: 15 }}
+							/>
+						</Pressable>
+					),
+				})}
+			/>
+			<BottomTab.Screen
+				name='TabTwo'
+				component={TabTwoScreen}
+				options={({ navigation }: any)=>({
+					title: "Tab Two",
+					tabBarIcon: ({ color }) => <TabBarIcon name='code' color={color} />,
+					headerRight: () => (
+						<Pressable
+							onPress={() => navigation.navigate("Modal")}
+							style={({ pressed }) => ({
+								opacity: pressed ? 0.5 : 1,
+							})}
+						>
+							<FontAwesome
+								name='info-circle'
+								size={25}
+								color={Colors[colorScheme].text}
+								style={{ marginRight: 15 }}
+							/>
+						</Pressable>
+					),
+				})}
+			/>
+		</BottomTab.Navigator>
+	);
 }
 
 /**
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,15 +2,20 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+export default function TabTwoScreen({ navigation }: RootTabScreenProps<"TabTwo">) {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab Two</Text>
+			<View
+				style={styles.separator}
+				lightColor='#eee'
+				darkColor='rgba(255,255,255,0.1)'
+			/>
+			<EditScreenInfo path='/screens/TabTwoScreen.tsx' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
